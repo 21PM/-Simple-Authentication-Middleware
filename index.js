d2384e649d917a78a66a92fe1d23728c10faf18f
21PM <parasmore33@gmail.com>
2024-07-09 15:35:08 +0530

    simple mw logger

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+
+const logger = (req, res, next) => {
+    const start = Date.now();
+    const { method, url } = req;
+    const timestamp = new Date().toISOString();
+
+    res.on('finish', () => {
+        const duration = Date.now() - start;
+        console.log(`[${timestamp}] ${method} ${url} - ${duration}ms`);
+    });
+
+    next();
+
+
+
+app.use(logger)
+
+app.get("/login", (req, res) => {
+    res.send("Hello World!");
+});
+
+app.listen(10000,()=>{
+    console.log("server is up at port number 10000")
+})
